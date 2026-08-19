bceed6c3aa81728e6dc05b2f8a7f90fffd3f34c7
dbs15329 <e1523503@u.nus.edu>
2026-08-20 01:25:32 +0800

    Add greeting and exit

---
+++ src/main/java/Nova.java
@@ -0,0 +1,12 @@
+public class Nova {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm Nova");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
