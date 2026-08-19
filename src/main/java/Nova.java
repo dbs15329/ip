fa79a4cb24fb7e2d41abfeffe9f4a1e21d9f703e
dbs15329 <e1523503@u.nus.edu>
2026-08-20 01:31:11 +0800

    Add support for echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(line);
         System.out.println(" Hello! I'm Nova");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+
+        sc.close();
     }
 }
